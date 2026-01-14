19cb15c5e5ad97253e26aaf1ddb1262c34bf75e3
Basirkhan418 <thebasirkhanofficial@gmail.com>
2026-01-14 22:43:20 +0530

    fixed

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
-import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { Logger } from 'winston';
 @Controller()
 export class AppController {
   
   constructor(
     private readonly appService: AppService,
-    @Inject(WINSTON_MODULE_NEST_PROVIDER) private readonly logger: Logger
+    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger
   ) {}
 
   @Get()
